59633ff7299441879e3c0f84b6e7c5a204571d80
Illya Halych <miagreengal@gmail.com>
2025-08-16 17:36:54 +0300

    modified some files for my site :)

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Post
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'bio')
+    search_fields = ('name',)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_date')
+    search_fields = ('title', 'content')
+    list_filter = ('author', 'published_date')
+    date_hierarchy = 'published_date'
+    ordering = ('-published_date',)
--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, default=None)
 
     def __str__(self):
         return self.title
